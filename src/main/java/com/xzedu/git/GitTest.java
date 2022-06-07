1f3d51a0f0d8aba83addaea17c5ace5944a7d34e
xz <2362989560@qq.com>
2022-06-07 21:04:41 +0800

    hot fix second commit

--- src/main/java/com/xzedu/git/GitTest.java
+++ src/main/java/com/xzedu/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
-
+        System.out.println("hhahahahh");
     }
 }
