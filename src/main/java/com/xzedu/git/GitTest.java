f39c8bebcddda2fa0d4043fa215ac73e9c41cb4f
firsttohere <106420210+firsttohere@users.noreply.github.com>
2022-06-07 21:43:41 +0800

    Update GitTest.java

--- src/main/java/com/xzedu/git/GitTest.java
+++ src/main/java/com/xzedu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hhahahahh");
+        System.out.println("show github");
     }
 }
